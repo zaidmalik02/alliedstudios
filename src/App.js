65a9f116f4bc59ae656dcc98d233149e8b91608f
zaidmalik02 <99412205+zaidmalik02@users.noreply.github.com>
2024-12-16 17:51:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css'; // Import global CSS for styling
 import { Route, Routes, BrowserRouter } from 'react-router-dom'; // for routing
@@ -21,27 +20,6 @@ function App() {
         </Routes>
       </div>
     </BrowserRouter>
-=======
-import React from "react";
-import { Route, Routes } from "react-router-dom";  // Import Routes and Route
-import Header from "./components/Header";  // Import the Header component
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-
-function App() {
-  return (
-    <div className="App">
-      <Header />  {/* Add Header to the App */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
->>>>>>> 7232a70679a4a4a696a15c11ad5d24d17154d074
   );
 }
 
