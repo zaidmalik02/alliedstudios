7232a70679a4a4a696a15c11ad5d24d17154d074
zaidmalik02 <99412205+zaidmalik02@users.noreply.github.com>
2024-12-16 17:20:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";  // Import BrowserRouter
+import { Route, Routes } from "react-router-dom";  // Import Routes and Route
 import Header from "./components/Header";  // Import the Header component
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -8,17 +8,15 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>  {/* Wrap your app with BrowserRouter */}
-      <div className="App">
-        <Header />  {/* Add Header to the App */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Header />  {/* Add Header to the App */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 }
 
